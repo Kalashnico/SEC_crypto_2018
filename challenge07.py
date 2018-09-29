e0b09e618a59ffe6548df0dd27982b45ed9cb88d
Nicolas Guerin <nicolas.guerin@epitech.eu>
2018-09-29 12:58:40 +0200

    fixed challenge 07

--- challenge07.py
+++ challenge07.py
@@ -11,8 +11,28 @@ unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 def read_file():
     file = open(sys.argv[1], 'rb')
-    file_data = file.readlines()
+    try:
+        file_data = file.readlines()
+    except UnicodeDecodeError:
+        sys.exit(84)
+
+    if len(file_data) != 2:
+        sys.exit(84)
+
     file_data[0] = file_data[0].rstrip()
+
+    # Verify if the key is valid.
+    try:
+        is_hexa = binascii.b2a_base64(binascii.unhexlify(file_data[0]))
+    except Exception:
+        sys.exit(84)
+
+    # Verify if the text is base64 encoded
+    try:
+        base64.decodebytes(file_data[1])
+    except binascii.Error:
+        sys.exit(84)
+
     return file_data
 
 
